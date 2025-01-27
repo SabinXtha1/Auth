6222c678065116f79728dd27f718b6bda2a35f17
Sabin Nayaju <raj123nayaju@gmail.com>
2025-01-27 14:29:32 +0545

    Dep

--- fontend/vite.config.js
+++ fontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       '/api':{
-        target:'http://localhost:4000',
+        target:`${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}`,
         changeOrigin:true,
       }
     }
